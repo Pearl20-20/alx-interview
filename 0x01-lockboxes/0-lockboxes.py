5dcfc117fce6064b8dc5c35dd877cc44349df335
Pearl20-20 <manneshie@yahoo.com>
2023-10-10 20:21:08 +0000

    Intetrview Quizes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """
-The variable N represents number of boxes
-The Boxes are arranged sequentially from'
-0 to n-1 and each box may contain keys to othe boxes
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially
+from 0 to n - 1 and each box may
+contain keys to the other boxes.
 """
 
 
-def canUnloackAll(boxes):
+def canUnlockAll(boxes):
     """
-    a function that determines if all the boxes can be opened
+     a method that determines if all the boxes can be opened.
 
     :param boxes:
     :return: True or False
     """
-
-    if not boxes or type(boxes) is not in list:
+    if not boxes or type(boxes) is not list:
         return False
 
     unlocked = [0]
     for n in unlocked:
-        for key in boxes(n):
+        for key in boxes[n]:
             if key not in unlocked and key < len(boxes):
                 unlocked.append(key)
     if len(unlocked) == len(boxes):
         return True
-    return false
+    return False
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+nimun Operations
